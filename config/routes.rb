5a11f0def3d80392e877f2720cb43e5c2da1adb2
Belen Battaglini <belenbattaglini@gmail.com>
2019-12-10 20:12:33 +0100

    changes to home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :artists,  path: 'artists'
   devise_for :users,  path: 'users'
   root to: 'pages#home'
+  resources :products do
+    resources :images
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
